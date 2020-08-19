8b14a08c2b818cd978e254e6f2ffd91e4499cb26
Eduard Manukyan <edman110296@gmail.com>
2020-08-19 20:51:34 +0400

    create page home and favorite

---
+++ home-page/home-page.js
@@ -0,0 +1,36 @@
+
+const loginBox = document.querySelector(".login-box")
+const textBox = document.querySelector(".textbox")
+const inputName = document.querySelector(".input-name")
+const inputPass = document.querySelector(".input-pass")
+const butt = document.querySelector("but")
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+window.onload = () => {
+    const loginElement = document.getElementById("login");
+    if (loginElement) {
+        loginElement.addEventListener("click", () => {
+            login();
+        });
+    }
+}
+
+const login = () => {
+    if (usernameValid() && passwordValid()) {
+        window.location = "../index.html";
+    }
+    else {
+    }
+}
+
+const usernameValid = () => {
+    return username.value === username.value.trim();
+}
+
+const passwordValid = () => {
+    return password.value.trim().length > 4;
+}
+
+
+
